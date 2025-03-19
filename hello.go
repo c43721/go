eef20fb71bb468d587533c2587fce0c992306ba1
c43721 <rlahman.game@gmail.com>
2025-03-18 21:10:06 -0500

    feat: generics are cool

--- hello.go
+++ hello.go
@@ -3,15 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	ints := []int64{1, 2, 3}
+	ints := []int{1, 2, 3}
+
+	bigInts := []int64{1, 2, 3, 4, 5}
 
 	sum := Sums(ints)
+	bigSum := Sums(bigInts)
 
-	fmt.Printf("Sum: %d", sum)
+	fmt.Printf("Sum ints: %d\n", sum)
+	fmt.Printf("Sum bigs: %d", bigSum)
 }
 
-func Sums(items []int64) int64 {
-	var sum int64
+func Sums[T int64 | int](items []T) T {
+	var sum T
 	for _, v := range items {
 		sum += v
 	}
